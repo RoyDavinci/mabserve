d86c1a89c5779e50c2249eb2cd970a9631276dfe
DavinciCode <emsthias33@gmail.com>
2023-12-10 15:10:08 +0100

    some information added

--- src/controllers/webhook/webhook.ts
+++ src/controllers/webhook/webhook.ts
@@ -1,7 +1,12 @@
 import { type Request, type Response } from 'express'
+import logger from '../../common/logger'
 
 export const webhook = async (req: Request, res: Response) => {
   try {
+    logger.error(req.params)
+    logger.error(req.query)
     return res.status(200).json({ data: req.params, item: req.query })
-  } catch (error) {}
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
 }
